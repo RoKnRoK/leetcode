3b33ef0bee2738a4c5ce619a9db7a697dc59f974
Roman Kulikov <roman.kulikov.ufa@gmail.com>
2016-07-19 00:11:10 +0500

    Добавил две задачки

---
+++ src/com/rok/leetcode/LongestSubstringWithoutRepeatingChars.java
@@ -0,0 +1,28 @@
+package com.rok.leetcode;
+
+/**
+ * Created by RoK on 19.07.2016.
+ * All rights reserved =)
+ */
+public class LongestSubstringWithoutRepeatingChars {
+    public int lengthOfLongestSubstring(String s) {
+        int strLength = s.length();
+        String saved = "";
+        String current = "";
+        for (int i = 0; i < strLength; i++) {
+            char character = s.charAt(i);
+            int firstIndex = current.indexOf(character);
+            if (firstIndex != -1) {
+                if (saved.length() < current.length()) {
+                    saved = current;
+                }
+                current = current.substring(firstIndex + 1);
+            }
+            current += character;
+        }
+        if (saved.length() < current.length()) {
+            saved = current;
+        }
+        return saved.length();
+    }
+}
