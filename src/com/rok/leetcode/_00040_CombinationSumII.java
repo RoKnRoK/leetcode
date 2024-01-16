8e4669cc149428b4e10967e90450153090b7d251
RoKnRoK <roman.kulikov.ufa@gmail.com>
2024-01-16 23:19:16 +0100

    Add intuitive solution for task #40, adjust solution for task #39

--- src/com/rok/leetcode/_00040_CombinationSumII.java
+++ src/com/rok/leetcode/_00040_CombinationSumII.java
@@ -1,29 +1,38 @@
 package com.rok.leetcode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by RoK on 19.07.2016.
  * All rights reserved =)
  */
-public class _00039_CombinationSum {
+public class _00040_CombinationSumII {
     public static void main(String[] args) {
-        printAnswer(combinationSum(new int[]{2, 3, 6, 7}, 7));
-        printAnswer(combinationSum(new int[]{2, 3, 5}, 8));
-        printAnswer(combinationSum(new int[]{2}, 3));
-        printAnswer(combinationSum(new int[]{2}, 1));
-        printAnswer(combinationSum(new int[]{1}, 10));
-        printAnswer(combinationSum(new int[]{1, 2}, 10));
+        printAnswer(combinationSum2(new int[]{2, 3}, 5));
+        printAnswer(combinationSum2(new int[]{10,1,2,7,6,1,5}, 8));
+        printAnswer(combinationSum2(new int[]{2,5,2,1,2}, 5));
+        printAnswer(combinationSum2(new int[]{2}, 3));
+        printAnswer(combinationSum2(new int[]{2}, 2));
+        printAnswer(combinationSum2(new int[]{2}, 1));
+        printAnswer(combinationSum2(new int[]{1}, 10));
+        printAnswer(combinationSum2(new int[]{1,2,3}, 10));
     }
 
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         return inner(candidates, 0, target);
     }
 
     private static List<List<Integer>> inner(int[] candidates, int from, int target) {
         List<List<Integer>> result = new ArrayList<>(20);
+        int previousCandidate = -1;
         for (int i = from; i < candidates.length; i++) {
+            if (candidates[i] == previousCandidate) {
+                continue;
+            }
+            previousCandidate = candidates[i];
             if (candidates[i] > target) {
                 return result;
             }
@@ -31,7 +40,7 @@ public class _00039_CombinationSum {
                 result.add(new ArrayList<>(List.of(candidates[i])));
                 return result;
             }
-            List<List<Integer>> previous = inner(candidates, i , target - candidates[i]);
+            List<List<Integer>> previous = inner(candidates, i+1, target - candidates[i]);
             if (previous.isEmpty()) {
                 continue;
             }
